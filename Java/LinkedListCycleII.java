74161a35d1cb6be238d04b624b62d2ca13fd545e
GabrielYeah <gabriel.yhs@gmail.com>
2014-10-28 20:37:56 -0700

    [Routine] Old math problem

---
+++ Java/LinkedListCycleII.java
@@ -0,0 +1,36 @@
+// Given a linked list, return the node where the cycle begins. If there is no cycle, return null.
+
+// Follow up:
+// Can you solve it without using extra space?
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+// Actually a math problem.
+// Try to figure out the relation between fast and slow node
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        ListNode fast = head, slow = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow) {
+                fast = head;
+                while (fast != slow) {
+                    fast = fast.next;
+                    slow = slow.next;
+                }
+                return fast;
+            }
+        }
+        return null;
+    }
+}
