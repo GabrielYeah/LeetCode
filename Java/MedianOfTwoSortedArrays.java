1ffa623bb53c953c0768f3a74bfba34db046ceea
Haishan Ye <gabriel.yhs@gmail.com>
2015-02-11 00:03:04 -0800

    [Update] An update on an interesting problem

--- Java/MedianOfTwoSortedArrays.java
+++ Java/MedianOfTwoSortedArrays.java
@@ -37,3 +37,35 @@ public class Solution {
         }
     }
 }
+
+// Updated version.
+// A quite different solution. In previous one, I am trying to pass down the updated
+// window down to next level. However, the cost would be that it is extremely confusing
+// when handling the index, and the count, and all those kind of stuff.
+// In this version, it is much simpler. Instead of removing half of the selected array
+// each time, this version is removing half of k elements in the front. Different from
+// the previous version, the right side of the arrays are not taken into consideration.
+// In this way, code is much more clear.
+public class Solution {
+    public double findMedianSortedArrays(int A[], int B[]) {
+        int len = A.length + B.length;
+        if (len % 2 == 1)
+            return findKth(A, 0, B, 0, len / 2 + 1);
+        else
+            return (findKth(A, 0, B, 0, len / 2) + findKth(A, 0, B, 0, len / 2 + 1)) / 2;
+    }
+    
+    private double findKth(int[] A, int startA, int[] B, int startB, int k) {
+        if (startA >= A.length) return B[startB + k - 1];
+        if (startB >= B.length) return A[startA + k - 1];
+        if (k <= 1) return Math.min(A[startA], B[startB]);
+        int midValA = startA + k / 2 - 1 < A.length ? A[startA + k / 2 - 1] : Integer.MAX_VALUE;
+        int midValB = startB + k / 2 - 1 < B.length ? B[startB + k / 2 - 1] : Integer.MAX_VALUE;
+        double res = 0;
+        if (midValA < midValB)
+            res = findKth(A, startA + k / 2, B, startB, k - k / 2);
+        else
+            res = findKth(A, startA, B, startB + k / 2, k - k / 2);
+        return res;
+    }
+}
